b7d433b45d0e995047b9bd7f4ed6f3daac6f9295
Sergey Benyaminyan <sbenyaminyan@gmail.com>
2017-03-19 12:55:31 +0400

    Project setup

---
+++ Lesson26/Lesson26/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson26
+//
+//  Created by Mac on 3/19/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
